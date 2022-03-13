082106ccd67ed2b300cf55c9f01c220151893a9b
cshimegi <cs-himegi@hotmail.com>
2022-03-13 23:28:14 +0900

    (#1) Add i18n

---
+++ lib/consts/locale_keys.g.dart
@@ -0,0 +1,15 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const app_name = 'app.name';
+  static const app_language_en = 'app.language.en';
+  static const app_language_ja = 'app.language.ja';
+  static const app_language_tw = 'app.language.tw';
+  static const app_language = 'app.language';
+  static const app = 'app';
+  static const common_language = 'common.language';
+  static const common = 'common';
+  static const button_start_game = 'button.start_game';
+  static const button = 'button';
+
+}
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:app/views/question.dart';
 import 'package:app/views/news.dart';
-import 'package:app/consts/system.dart';
+import 'package:app/consts/locale_keys.g.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -34,11 +35,64 @@ class HomeState extends State<HomeScreen> {
     });
   }
 
+  List<PopupMenuEntry> buildPopupMenuItems(BuildContext context) {
+    return [
+      PopupMenuItem(
+        child: Text(LocaleKeys.app_language_en.tr()),
+        value: 'en',
+      ),
+      PopupMenuItem(
+        child: Text(LocaleKeys.app_language_ja.tr()),
+        value: 'ja',
+      ),
+      PopupMenuItem(
+        child: Text(LocaleKeys.app_language_tw.tr()),
+        value: 'tw',
+      )
+    ];
+  }
+
+  void setSelectedLanguage(Locale locale) async {
+    await context.setLocale(locale);
+  }
+
+  void onSelectedLocale(String value) {
+    Locale locale;
+    switch (value.toString()) {
+      case 'tw':
+        locale = const Locale('zh', 'hant');
+        break;
+      case 'ja':
+        locale = const Locale('ja');
+        break;
+      default:
+        locale = const Locale('en');
+        break;
+    }
+    setSelectedLanguage(locale);
+  }
+
   AppBar buildAppBar() {
     return AppBar(
-      title: const Text(appName),
-      actions: const [
-        Padding(
+      title: Text(LocaleKeys.app_name.tr()),
+      actions: [
+        PopupMenuButton(
+            elevation: 8,
+            icon: const Icon(Icons.language),
+            offset: Offset(0.0, AppBar().preferredSize.height),
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(8.0),
+                bottomRight: Radius.circular(8.0),
+                topLeft: Radius.circular(8.0),
+                topRight: Radius.circular(8.0),
+              ),
+            ),
+            onSelected: (value) {
+              onSelectedLocale(value.toString());
+            },
+            itemBuilder: buildPopupMenuItems),
+        const Padding(
           padding: EdgeInsets.symmetric(horizontal: 12),
           child: Icon(Icons.logout),
         ),
